7c4b81b56af4d044c775a27bef107cbf7f07570d
Omar <omar231120@hotmail.com>
2022-03-15 18:41:49 -0600

    MovieReader implementado, y primeras implementaciones de quicksort

--- src/Test/App.java
+++ src/Test/App.java
@@ -1,7 +1,30 @@
 package test;
 
+import java.util.ArrayList;
+
+import model.Movie;
+import utils.MovieReader;
+import utils.Quicksort;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        MovieReader movieReader = new MovieReader();
+        Quicksort quicksort = new Quicksort();
+
+        ArrayList<Movie> movies = new ArrayList<Movie>(); // Create an ArrayList Movie
+        
+        for(int i= 0; i< 10;i++){
+            movies.add(movieReader.readMovie(i));
+        }
+        
+
+
+        // int[] ordenado = quicksort.order(numeros, 0, numeros.length-1);
+
+        // for(Integer value: ordenado){
+        //     System.out.println(value);
+        // }
         
     }
 }
--- src/utils/Quicksort.java
+++ src/utils/Quicksort.java
@@ -1,5 +1,33 @@
 package utils;
 
 public class Quicksort {
-    
+
+    public int[] order(int A[], int left, int right) {
+        int pivot = A[left]; // tomamos primer elemento como pivote
+        int i = left; // i realiza la búsqueda de izquierda a derecha
+        int j = right; // j realiza la búsqueda de derecha a izquierda
+        int aux;
+
+        while (i < j) { // mientras no se crucen las búsquedas
+            while (A[i] <= pivot && i < j)
+                i++; // busca elemento mayor que pivote
+            while (A[j] > pivot)
+                j--; // busca elemento menor que pivote
+            if (i < j) { // si no se han cruzado
+                aux = A[i]; // los intercambia
+                A[i] = A[j];
+                A[j] = aux;
+            }
+        }
+
+        A[left] = A[j]; // se coloca el pivote en su lugar de forma que tendremos
+        A[j] = pivot; // los menores a su izquierda y los mayores a su derecha
+
+        if (left < j - 1)
+            order(A, left, j - 1); // ordenamos subarray izquierdo
+        if (j + 1 < right)
+            order(A, j + 1, right); // ordenamos subarray derecho
+
+        return A;
+    }
 }
