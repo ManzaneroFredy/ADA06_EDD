5686eb4e86c0f668ffb311305d7f7e3d25d8aabd
Chango <a16001581@alumnos.uady.mx>
2022-03-18 15:03:32 -0600

    Se agrego el objeto archivo para sacar el archivo de salida, así mismo se adecuo el codigo para que el usuario pueda ingresar la dirección de salida

---
+++ src/utils/Archivo.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import model.Movie;
+
+
+public class Archivo{
+
+
+    public void generarArchivoSalida(List<Movie> listaMovie, String direccionDeSalida) throws Exception{
+        FileOutputStream archivoCsv = new FileOutputStream(direccionDeSalida);
+        OutputStreamWriter salida = new OutputStreamWriter(archivoCsv, "UTF-8");
+
+
+        for(Movie movie: listaMovie){
+            salida.write(movie.getId() + ", " + movie.getMovie_title() + ", " + movie.getDuration()+ ", " + movie.getColor()+ ", " + movie.getLanguage()
+            + ", " + movie.getCountry()+ ", " + movie.getContent_rating()+ ", " + movie.getBudget()+ ", " + movie.getYear()+ ", " + movie.getImdb_score()
+            + ", " + movie.getAspect_ratio()+ ", " + movie.getMovie_imdb_link() + "\n");
+        }
+        
+        salida.close();
+    }
+
+  
+}
